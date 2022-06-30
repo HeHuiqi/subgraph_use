869a893592a68337520ca2859f446c2c190ac1e4
HeHuiqi <1710308677@qq.com>
2022-06-30 15:00:28 +0800

    update hqtoken

--- scripts/hqtoken-script.js
+++ scripts/hqtoken-script.js
@@ -12,36 +12,6 @@ const getArtifactPath = async function(contractName) {
   return path.join(hre.config.paths.artifacts, artifact.sourceName, `${artifact.contractName}.json`)
 }
 
-
-async function deployMarket() {
-
-  const HqMarket = await hre.ethers.getContractFactory("HqMarket");
-  const hqMarket = await HqMarket.deploy();
-  await hqMarket.deployed();
-  const [main,other] = await ethers.getSigners();
-
-  console.log("hqMarket deployed to:", hqMarket.address);
-
-  let uint = BigNumber.from("1000000000000000000");
-
-  let num = BigNumber.from("2022")
-  let amount = num.mul(uint);
-  let tx = await hqMarket.mint(main.address,amount.toHexString());
-  tx = await tx.wait();
-  let mBalance = await hqMarket.balanceOf(main.address);
-
-
-  amount = amount.div(BigNumber.from('2'));
-  tx = await hqMarket.transfer(other.address, amount.toHexString());
-  mBalance = await hqMarket.balanceOf(main.address);
-  let oBalance = await hqMarket.balanceOf(other.address);
-
-  console.log("abi:",abi);
-    //这里返回合约的名称和地址作为subgraph项目的初始化参数
-  return { contractName: "HqMarket", address: hqMarket.address,abi:abi}
-}
-
-
 async function deployToken() {
 
   const HqToken = await hre.ethers.getContractFactory("HqToken");
